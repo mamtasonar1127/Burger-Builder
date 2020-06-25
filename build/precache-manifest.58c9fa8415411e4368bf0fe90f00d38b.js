bddc8e7751a7302b287ab3a2e445c627f5dbec2e
mamtasonar1127 <46754967+mamtasonar1127@users.noreply.github.com>
2020-06-25 11:48:43 -0400

    file setup final commit

---
+++ build/precache-manifest.58c9fa8415411e4368bf0fe90f00d38b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ff9fc3a0140cd6aa98ebf4ef09441b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2c92b31e13f91d3823b",
+    "url": "/static/css/main.23248127.chunk.css"
+  },
+  {
+    "revision": "3ec09ada6a96245564c5",
+    "url": "/static/js/2.78c32d84.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.78c32d84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2c92b31e13f91d3823b",
+    "url": "/static/js/main.f96322e5.chunk.js"
+  },
+  {
+    "revision": "e1e5c78d49d83331038d",
+    "url": "/static/js/runtime-main.6ea168cc.js"
+  }
+]);
